52f399e990a48e44882edc2b5a8f8af7485d6f4f
michael.bern.gasendo <mike101290@gmail.com>
2022-12-02 11:51:40 +0800

    Added buttons

--- app/src/main/java/acn/gasendo/randomjokesapplication/presentation/home/HomeActivity.kt
+++ app/src/main/java/acn/gasendo/randomjokesapplication/presentation/home/HomeActivity.kt
@@ -7,16 +7,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import acn.gasendo.randomjokesapplication.presentation.ui.theme.PostsKtorClientTheme
+import acn.gasendo.randomjokesapplication.presentation.ui.theme.RandomJokeApplicationTheme
+import acn.gasendo.randomjokesapplication.presentation.ui.theme.ThemeState
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
@@ -28,40 +32,108 @@ class HomeActivity : ComponentActivity() {
             val viewmodel = hiltViewModel<HomeViewModel>()
             val state = viewmodel.state.value
 
-            PostsKtorClientTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.LightGray),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    if (state.posts != null) { // success
-                        LazyColumn(modifier = Modifier.fillMaxSize()) {
-                            items(state.posts) {
-                                Card(
-                                    modifier = Modifier
-                                        .padding(horizontal = 7.dp, vertical = 3.dp)
-                                        .fillMaxWidth(),
-                                    backgroundColor = Color.White
-                                ) {
-                                    Text(
-                                        text = it.setup + "\n\n" + it.punchline + "\n\n" + "Subject: " + it.type,
-                                        modifier = Modifier.padding(10.dp)
-                                    )
+            RandomJokeApplicationTheme {
+
+                val scaffoldState = rememberScaffoldState()
+                Surface {
+                    Scaffold(
+                        topBar = {
+                            AppBar()
+                        },
+                        scaffoldState = scaffoldState,
+                        content = {
+                            Column(
+                                modifier = Modifier
+                                    .background(MaterialTheme.colors.background),
+                                verticalArrangement = Arrangement.Bottom,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                if (state.posts != null) { // success
+                                    LazyColumn(modifier = Modifier.fillMaxSize()) {
+                                        items(state.posts) {
+                                            Card(
+                                                modifier = Modifier
+                                                    .padding(horizontal = 7.dp, vertical = 3.dp)
+                                                    .fillMaxWidth(),
+                                                backgroundColor = Color.White
+                                            ) {
+                                                Text(
+                                                    text = it.setup + "\n\n" + it.punchline + "\n\n" + "Subject: " + it.type,
+                                                    modifier = Modifier.padding(10.dp)
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                } else {
+                                    if (state.loading) {
+                                        CircularProgressIndicator()
+                                    } else {
+                                        state.error?.let { Text(text = it) }
+                                    }
                                 }
                             }
-                        }
 
-                    } else {
-                        if (state.loading) {
-                            CircularProgressIndicator()
-                        } else {
-                            state.error?.let { Text(text = it) }
-                        }
-                    }
+                        },
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+fun AppBar() {
+
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+
+            ) {
+                Button(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .border(
+                            2.dp,
+                            color = MaterialTheme.colors.primary,
+                            shape = RoundedCornerShape(5.dp)
+                        ),
+                    onClick = { /*TODO*/ })
+                {
+
+                    Text(text = "Reload")
+                }
+                Button(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .border(
+                            2.dp,
+                            color = MaterialTheme.colors.primary,
+                            shape = RoundedCornerShape(5.dp)
+                        ),
+                    onClick = { /*TODO*/ })
+                {
+                    Text(text = "Favorites")
+                }
+                Button(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .border(
+                            2.dp,
+                            color = MaterialTheme.colors.primary,
+                            shape = RoundedCornerShape(5.dp)
+                        ),
+                    onClick = { ThemeState.isLight = !ThemeState.isLight })
+                {
+                    Text(text = "Theme")
+                }
+            }
+
+        }
+
+    )
 }
--- app/src/main/java/acn/gasendo/randomjokesapplication/presentation/ui/theme/Theme.kt
+++ app/src/main/java/acn/gasendo/randomjokesapplication/presentation/ui/theme/Theme.kt
@@ -1,44 +1,45 @@
 package acn.gasendo.randomjokesapplication.presentation.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
+private val DarkThemeColors = darkColors(
+    primary = Color.White,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
+private val LightThemeColors = lightColors(
+    primary = Color.Black,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
+
+
+object ThemeState {
+    var isLight by mutableStateOf(true)
+}
+
 
 @Composable
-fun PostsKtorClientTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun RandomJokeApplicationTheme(
+    children: @Composable() () -> Unit
+) {
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    if (ThemeState.isLight) {
+        MaterialTheme(colors = LightThemeColors) {
+            children()
+        }
+    } else {
+        MaterialTheme(colors = DarkThemeColors) {
+            children()
+        }
+
+    }
 }
